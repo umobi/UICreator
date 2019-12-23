d1ceb8e83d007103c427f2f7db714f24497d750d
brennoumobi <brenno@umobi.com.br>
2019-12-23 20:15:41 -0300

    Use LivePreview from SwiftUI
    - Works only for projects that has iOS Development Target greater than iOS 13.0.
    
    Code Snippets:
    
    struct MyView_LivePreview: PreviewProvider {
        static var previews: some SwiftUI.View {
            LivePreview<MyView>()
        }
    }

---
+++ UICreator/Classes/Previewer.swift
@@ -0,0 +1,33 @@
+//
+//  Previewer.swift
+//  UICreator
+//
+//  Created by brennobemoura on 23/12/19.
+//
+
+import Foundation
+
+#if canImport(SwiftUI)
+import SwiftUI
+
+@available(iOS 13, *)
+public struct LivePreview<View: ViewBuilder>: SwiftUI.View {
+    public init() {}
+    
+    public var body: some SwiftUI.View {
+        Previewer<View>()
+    }
+}
+
+@available(iOS 13, *)
+public struct Previewer<View: ViewBuilder>: UIViewRepresentable {
+    public init() {}
+    
+    public func makeUIView(context: Context) -> View {
+        return View()
+    }
+
+    public func updateUIView(_ uiView: View, context: Context) {}
+}
+
+#endif
