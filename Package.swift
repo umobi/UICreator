9e0faba2669e5b5244c923759d6c2530a8301e54
brennoumobi <brenno@umobi.com.br>
2020-10-28 17:54:15 -0300

    Updated ConstraintBuilder

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["UICreator"])
     ],
     dependencies: [
-        .package(url: "https://github.com/umobi/ConstraintBuilder", .upToNextMajor(from: "2.0.0"))
+        .package(url: "https://github.com/umobi/ConstraintBuilder", .upToNextMajor(from: "2.0.1"))
     ],
     targets: [
         .target(
